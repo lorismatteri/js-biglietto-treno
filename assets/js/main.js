11565a0970254f0bbefa8d65bd7168f797516503
Loris Matteri <loris.matteri93@gmail.com>
2020-11-13 16:51:49 +0100

    Prezzo con sconto 40%
    Aggiunte anche due stringhe per abbellire risultato

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 // Chiedere all'utente il numero di chilometri che vuole percorrere
-var kmPercorsi, userAge, kmPrice, prezzo, scontoVenti;
+var kmPercorsi, userAge, kmPrice, prezzo, scontoVenti, scontoVecchi;
 
 kmPercorsi = Number(prompt("Quanti km devi percorrere?"));
 
@@ -14,12 +14,17 @@ kmPrice = 0.21;
 scontoVenti = kmPercorsi * kmPrice * 0.2;
 
 // va applicato uno sconto del 40% per gli over 65
+scontoVecchi = kmPercorsi * kmPrice * 0.4;
 
-if (userAge >= 18 && userAge <=65 ) {
+//Siamo gentili e arrotondiamo per difetto ;)
+
+if (userAge >= 18 && userAge < 65) {
   prezzo = Math.floor(kmPercorsi * kmPrice);
 } else if (userAge < 18) {
   prezzo = Math.floor(kmPercorsi * kmPrice - scontoVenti);
+} else if (userAge >= 65) {
+  prezzo = Math.floor(kmPercorsi * kmPrice - scontoVecchi);
 }
 
-console.log(kmPrice, kmPercorsi, userAge, prezzo);
-document.getElementById("price").innerHTML = prezzo;
+console.log(kmPrice, kmPercorsi, userAge, prezzo, scontoVenti, scontoVecchi);
+document.getElementById("price").innerHTML = "Il tuo biglietto costa: " + prezzo + "€, arrivederci e buon viaggio!";
